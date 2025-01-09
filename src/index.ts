fe13fee8f8bac8fdfc5e5fc0cc4df95eed3c7250
Abdulhafeez <laxhameedhafeez@gmail.com>
2025-01-09 14:20:10 +0100

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express from "express";
 import routes from "./routes";
 import * as dotenv from "dotenv";
@@ -8,6 +9,7 @@ const app = express();
 const port = process.env.PORT || 3030;
 
 //middleware
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/images", routes)
